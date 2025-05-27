3198a443ab4b197371ba53e647774c76a0ffc692
Gyebran <152045520+Gyebran@users.noreply.github.com>
2025-05-27 13:09:18 +0700

    Inital Commit

---
+++ routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout',[AuthController::class, 'logout'])->middleware('auth:sanctum');
