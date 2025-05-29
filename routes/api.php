26beda4a4c59d4bff3a2c6ad5a56f550a2590f96
altppp <ahmadfaizalthaf@gmail.com>
2025-05-29 21:49:54 +0700

    progres 2

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+use App\Http\Controllers\CommentController;
+
+Route::middleware(['auth'])->group(function () {
+    Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::put('/comments/{comment}', [CommentController::class, 'update'])->name('comments.update');
+    Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
+});
+
