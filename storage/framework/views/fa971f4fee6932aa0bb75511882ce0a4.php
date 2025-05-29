50a61ef1dd6f5acaba204fe489bf6fd19a6c08b1
Ilbagas <ilhambagaska046@gmail.com>
2025-05-29 18:40:59 +0700

    front end dan controller login form

---
+++ storage/framework/views/fa971f4fee6932aa0bb75511882ce0a4.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="stylelogin.css">
+    <style>
+        body, html {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+            background-image: url('/images/bg.jpg');
+            background-size: cover;
+            background-position: center;
+        }
+    </style>
+    </head>
+<body>
+
+    <!--main container-->
+    <div class="container d-flex justify-content-center align-items-center min-vh-100" id="cont">
+
+
+    <!--login container-->
+        <div class="row border rounded-5 p-3 bg-white shadow box-area">
+
+    <!--bbox kiri-->
+        <div class="col-md-6 rounded-5 d-flex justify-content-center align-items-center flex-column left-box" style="background: white;">
+            <div class="featured-image mb-3">
+                <img src="<?php echo e(asset('images/Logo.png')); ?>" class="img-fluid rounded-bottom-5" style="width: 250px;">
+            </div>
+            <p class="text-black fs-2" style="font-weight: 600; padding-top: 10px;">Welcome Back!</p>
+        </div>  
+    <!--box kanan-->
+        <div class="col-md-6">
+            <div class="row align-items-center">
+                <div class="header-text  mt-3">
+                    <p>Enter your Email and Password.</p>
+                </div>
+
+                <form action="<?php echo e(url('/login')); ?>" method="POST">
+                    <?php echo csrf_field(); ?>
+
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email address ">
+                    </div>
+                        <div class="input-group mb-3">
+                        <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password ">
+                    </div>
+                    <div class="input-group mb-3">
+                        <button class="btn btn-lg btn-primary w-100 fs-6">Login</button>
+                    </div>
+
+                </form>
+                <div class="row">
+                    <small>Don't have account? <a href="#">Sign Up</a></small>
+                </div>
+                
+            </div>
+
+        </div>  
+
+        </div>
+
+    </div>
+</body>
+</html>
+<?php /**PATH C:\laragon\www\Tubes PAW\TUBES_PAW_KELOMPOK3\resources\views/auth/login.blade.php ENDPATH**/ ?>
