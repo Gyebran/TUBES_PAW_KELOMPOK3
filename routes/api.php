06b8c735899629a32df2002a5412fe078d00a2e7
Gyebran <152045520+Gyebran@users.noreply.github.com>
2025-05-31 13:49:28 +0700

    Initial Commit

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,15 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout',[AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::middleware('auth:sanctum')->put('/profile', [UserController::class, 'updateProfile']);
+Route::middleware(['auth:sanctum'])->get('/profile', [UserController::class, 'getProfile']);
 Route::middleware(['auth:sanctum', IsAdmin::class])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'index']);
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile/edit', function () {
+        return view('profile.edit');
+    })->name('profile.edit');
+
+    Route::post('/profile/update', [UserController::class, 'updateProfile'])->name('profile.update');
 });
