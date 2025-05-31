98f9b699f86eea724a32275e65f2b5f9fe97e0d1
altppp <ahmadfaizalthaf@gmail.com>
2025-05-31 15:39:25 +0700

    progres

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -7,14 +7,13 @@
     <button type="submit">Send</button>
 </form>
 
-<!-- Daftar Komentar -->
 @foreach ($karya->comments as $comment)
     <div>
         <strong>{{ $comment->user->name }}</strong>
         <p>{{ $comment->content }}</p>
 
         @if(auth()->id() === $comment->user_id || auth()->user()->is_admin)
-            <!-- Tombol Edit & Hapus -->
+           
         @endif
     </div>
 @endforeach
