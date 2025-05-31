cb8d6df33ec61c2cfa8be578a0cbb61eb0018384
Gyebran <152045520+Gyebran@users.noreply.github.com>
2025-05-31 13:15:44 +0700

    Initial Commit

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Middleware\IsAdmin;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout',[AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::middleware('auth:sanctum')->put('/profile', [UserController::class, 'updateProfile']);
+Route::middleware(['auth:sanctum', IsAdmin::class])->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'index']);
+});
