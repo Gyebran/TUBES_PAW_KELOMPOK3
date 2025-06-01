a590615eb671daf7b53bfd4348561f38d198be97
Gyebran <152045520+Gyebran@users.noreply.github.com>
2025-06-01 13:18:29 +0700

    Initial Commit

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\IsAdmin;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
@@ -7,17 +8,11 @@ use App\Http\Controllers\AdminController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout',[AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::middleware('auth:sanctum')->put('/profile', [UserController::class, 'updateProfile']);
-Route::middleware(['auth:sanctum'])->get('/profile', [UserController::class, 'getProfile']);
-Route::middleware(['auth:sanctum', IsAdmin::class])->group(function () {
-    Route::get('/admin/dashboard', [AdminController::class, 'index']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/profile', [UserController::class, 'getProfile']);
+    Route::put('/profile', [UserController::class, 'updateProfile']);
 });
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/profile/edit', function () {
-        return view('profile.edit');
-    })->name('profile.edit');
-
-    Route::post('/profile/update', [UserController::class, 'updateProfile'])->name('profile.update');
-});
+Route::middleware(['auth:sanctum', IsAdmin::class])->get('/admin/dashboard', [AdminController::class, 'index']);
